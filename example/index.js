973c76d9769dea1cfacd386926f112bbb761a3dd
Ruchira (Richie) Jayasekara <ruchira088@gmail.com>
2017-03-16 18:23:10 +1100

    Modifying README.md

--- example/index.js
+++ example/index.js
@@ -19,3 +19,23 @@ console.log(diusTennis.getScore(afterPointWon_2))
 // Prints "Adam-David, sets: 0-0, games: 0-0, points: 30-15"
 const afterPointWon_3 = diusTennis.playerWonPoint(player_2)(afterPointWon_2)
 console.log(diusTennis.getScore(afterPointWon_3))
+
+// Prints "Adam-David, sets: 0-0, games: 0-0, points: 30-30"
+const afterPointWon_4 = diusTennis.playerWonPoint(player_2)(afterPointWon_3)
+console.log(diusTennis.getScore(afterPointWon_4))
+
+// Prints "Adam-David, sets: 0-0, games: 0-0, points: 40-30"
+const afterPointWon_5 = diusTennis.playerWonPoint(player_1)(afterPointWon_4)
+console.log(diusTennis.getScore(afterPointWon_5))
+
+// Prints "Adam-David, sets: 0-0, games: 0-0, points: DEUCE"
+const afterPointWon_6 = diusTennis.playerWonPoint(player_2)(afterPointWon_5)
+console.log(diusTennis.getScore(afterPointWon_6))
+
+// Prints "Adam-David, sets: 0-0, games: 0-0, points: ADVANTAGE Adam"
+const afterPointWon_7 = diusTennis.playerWonPoint(player_1)(afterPointWon_6)
+console.log(diusTennis.getScore(afterPointWon_7))
+
+// Prints "Adam-David, sets: 0-0, games: 1-0, points: 0-0"
+const afterPointWon_8 = diusTennis.playerWonPoint(player_1)(afterPointWon_7)
+console.log(diusTennis.getScore(afterPointWon_8))
